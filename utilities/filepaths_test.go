34771bf927709f6903b980d8249017cef019b1f4
Chris Simpkins <git.simpkins@gmail.com>
2018-02-23 18:22:32 -0500

    added utilities/filepaths.go tests

---
+++ utilities/filepaths_test.go
@@ -0,0 +1,44 @@
+package utilities
+
+import (
+	"testing"
+	"path/filepath"
+
+)
+
+
+func TestSwapExtensionToGo(t *testing.T) {
+	tests := []struct{
+		inPath string
+		expectedOutPath string
+	}{
+		{"test.gor", "test.go"},
+		{filepath.Join("onedeep", "test.gor"), filepath.Join("onedeep", "test.go")},
+		{filepath.Join("twodeep", "onedeep", "test.gor"), filepath.Join("twodeep", "onedeep", "test.go")},
+	}
+
+	for _, testcase := range tests {
+		testPath := SwapExtension(testcase.inPath, "go")
+		if testPath != testcase.expectedOutPath {
+			t.Errorf("[FAIL] Returned path with %s inpath did not return expected outpath. Returned outpath: %s", testcase.inPath, testPath)
+		}
+	}
+}
+
+func TestSwapExtensionToGor(t *testing.T) {
+	tests := []struct{
+		inPath string
+		expectedOutPath string
+	}{
+		{"test.go", "test.gor"},
+		{filepath.Join("onedeep", "test.go"), filepath.Join("onedeep", "test.gor")},
+		{filepath.Join("twodeep", "onedeep", "test.go"), filepath.Join("twodeep", "onedeep", "test.gor")},
+	}
+
+	for _, testcase := range tests {
+		testPath := SwapExtension(testcase.inPath, "gor")
+		if testPath != testcase.expectedOutPath {
+			t.Errorf("[FAIL] Returned path with %s in path did not return expected outpath. Returned outpath: %s", testcase.inPath, testPath)
+		}
+	}
+}
